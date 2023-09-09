de84d72136504a186b0ae78fa1ec084a7da3402f
Farhan Angullia <farhanangullia@gmail.com>
2023-09-10 03:08:52 +0800

    update db init

--- catalog-service/app/entrypoints/api/middleware/configuration.py
+++ catalog-service/app/entrypoints/api/middleware/configuration.py
@@ -2,7 +2,7 @@ from app.adapters.models import pgsql_model
 from app.entrypoints.api.helpers.responses import *
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import Session, SQLModel, create_engine, select
+from sqlmodel import Session, SQLModel, create_engine
 
 
 def configure_cors(app: FastAPI) -> None:
@@ -24,50 +24,41 @@ def configure_health_checks(app: FastAPI) -> None:
 
 def init(db_uri: str):
     engine = create_engine(db_uri, echo=True)
-    products = None
     with Session(engine) as session:
-        statement = select(pgsql_model.Product)
-        results = session.exec(statement)
-        products = results.first()
+        if pgsql_model.Product.metadata.tables[
+            pgsql_model.Product.__tablename__
+        ].exists(engine):
+            return engine
+        else:
+            SQLModel.metadata.create_all(engine)
+            p1 = pgsql_model.Product(
+                name="Gameboy",
+                description="This device is a great piece of nintendo nostalgia for gamers",
+                price=50,
+                image="",
+            )
+            p2 = pgsql_model.Product(
+                name="Switch",
+                description="Enjoy a flexible gaming experience on this powerful, versatile console.",
+                price=200,
+                image="",
+            )
+            p3 = pgsql_model.Product(
+                name="Xbox Series X",
+                description="The fastest, most powerful Xbox ever.",
+                price=729,
+                image="",
+            )
+            p4 = pgsql_model.Product(
+                name="Playstation 5",
+                description="Marvel at incredible graphics and experience new PS5 features.",
+                price=850,
+                image="",
+            )
+            session.add_all([p1, p2, p3, p4])
+            session.commit()
 
-    if products:
-        return engine
-
-    SQLModel.metadata.drop_all(engine)
-    SQLModel.metadata.create_all(engine)
-    p1 = pgsql_model.Product(
-        name="Gameboy",
-        description="This device is a great piece of nintendo nostalgia for gamers",
-        price=50,
-        image="",
-    )
-    p2 = pgsql_model.Product(
-        name="Switch",
-        description="Enjoy a flexible gaming experience on this powerful, versatile console.",
-        price=200,
-        image="",
-    )
-    p3 = pgsql_model.Product(
-        name="Xbox Series X",
-        description="The fastest, most powerful Xbox ever.",
-        price=729,
-        image="",
-    )
-    p4 = pgsql_model.Product(
-        name="Playstation 5",
-        description="Marvel at incredible graphics and experience new PS5 features.",
-        price=850,
-        image="",
-    )
-
-    with Session(engine) as session:
-        session.add(p1)
-        session.add(p2)
-        session.add(p3)
-        session.add(p4)
-        session.commit()
-
-    return engine
+            return engine
 
 
 def configure_logging() -> None:
